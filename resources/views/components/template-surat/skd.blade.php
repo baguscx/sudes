6d0bda23e76852c465f6724697251c5cd21ffc97
Bagus Cahyono <baguscy8@gmail.com>
2024-05-07 22:24:29 +0700

    finishing warga feature

--- resources/views/components/template-surat/skd.blade.php
+++ resources/views/components/template-surat/skd.blade.php
@@ -178,7 +178,7 @@
         <br>
         <br>
         <div class="qr-container">
-            <img style="width: 25px" src="logo.png" class="lego" alt="">
+            <img style="width: 20px" src="logo.png" class="lego" alt="">
             <img class="object-a" src="data:image/png;base64, {!! base64_encode(QrCode::size(100)->generate('http://127.0.0.1:8000/cek/surat/'.$list->id)) !!} ">
         </div>
             {{-- <div class="object-a">{!! $qrCodes !!}</div> --}}
